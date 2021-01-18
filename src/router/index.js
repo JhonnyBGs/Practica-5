3acf707b4ebb1a7fd20c7f7a972e742d16525423
JhonnyBGs <73123155+JhonnyBGs@users.noreply.github.com>
2021-01-18 17:14:56 +0100

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,75 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// Views
+import Interface from "../views/Interface.vue";
+import Home from "../views/Home.vue";
+import MovieInfo from "../views/MovieInfo.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+
+//Componentes
+import Teaser from "@/components/Teaser";
+import FilmImages from "@/components/FilmImages";
+import FilmVideos from "@/components/FilmVideos";
+
+//Logic
+import auth from "@/logic/auth";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/auth/login",
+    name: "Login",
+    component: Login,
+    beforeEnter: (to, from, next) => {
+      if (auth.isLoggedIn()) next(from);
+      else next();
+    },
+  },
+  {
+    path: "/auth/register",
+    name: "Register",
+    component: Register,
+    beforeEnter: (to, from, next) => {
+      if (auth.isLoggedIn()) next(from);
+      else next();
+    },
+  },
+  {
+    path: "/",
+    component: Interface,
+    children: [{
+      path: "",
+      component: Home
+    }]
+  },
+  {
+    path: "/movie/:id/info",
+    component: Interface,
+    children: [{
+      path: "",
+      component: MovieInfo,
+      children: [
+        {
+          path: "", name: "teaser", component: Teaser
+        },
+        {
+          path: "", name: "images", component: FilmImages
+        },
+        {
+          path: "", name: "videos", component: FilmVideos
+        }
+      ]
+    }]
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="padding-top: 100px">
+    <b-container fluid="sm">
+      <b-media right-align vertical-align="center">
+        <template #aside>
+          <b-button variant="light"
+            ><b-icon icon="plus"></b-icon> Añadir Película</b-button
+          >
+        </template>
+        <h2 class="mt-0 mb-1">Listado de peliculas</h2>
+      </b-media>
+      <div fluid class="mt-4">
+        <filmslider
+          title="Más popular hoy"
+          url="https://api.themoviedb.org/3/trending/all/day"
+        ></filmslider>
+        <filmslider
+          title="Series más populares hoy"
+          url="https://api.themoviedb.org/3/trending/tv/day"
+        ></filmslider>
+        <filmslider
+          title="Peliculas mejor valoradas"
+          url="https://api.themoviedb.org/3/movie/top_rated"
+        ></filmslider>
+      </div>
+    </b-container>
+  </div>
+</template>
+<script>
+import filmslider from "@/components/SliderFilms";
+export default {
+  components: {
+    filmslider,
+  },
+};
+</script>
